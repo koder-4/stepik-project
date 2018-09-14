e7ef39c414045eeb20be9fdf11a80b85884eff92
Vitaliy Murysev <v.murysev@gmail.com>
2018-09-14 14:53:54 +0300

    gunicorn hello-world added

--- etc/install.sh
+++ etc/install.sh
@@ -3,3 +3,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/default /etc/nginx/sites-enabled/site.conf
 sudo service nginx stop
 sudo service nginx start
+gunicorn hello:app --config etc/gunicorn.py
