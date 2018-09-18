473c7db189b99b491e78a0e8e1a67f9717bfd515
Koder Sohm <k0der85@mail.ru>
2018-09-18 16:18:55 +0300

    first django commit

---
+++ ask/qa/urls.py
@@ -0,0 +1,11 @@
+from qa.views import test
+from django.urls import path, re_path
+
+
+urlpatterns = [
+    re_path(r'^$', test, name='root'),
+    path('login/', test, name='login'),
+    path('signup/', test, name='signup'),
+    path('question/<dec>/', test, name='question'),
+    path('popular/', test, name='new'),
+    path('new/', test, name='new')]
