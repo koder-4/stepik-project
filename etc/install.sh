228507c5b196ce66017c6b175e2174724561adc7
Koder Sohm <k0der85@mail.ru>
2018-09-28 11:53:20 +0000

    0,5 changes

--- etc/install.sh
+++ etc/install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "CREATE DATABASE stepik;"
+sudo mysql -uroot -e "CREATE USER stepik IDENTIFIED BY 'password1';"
+sudo mysql -uroot -e "GRANT ALL on stepik.* to stepik;"
 sudo dpkg --purge python-django
-sudo pip3 uninstal Django
-sudo pip3 install Django==2.0 --update
+sudo pip3 uninstall Django
+sudo pip3 install Django==2.0 --upgrade
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/default /etc/nginx/sites-enabled/site.conf
 sudo service nginx stop
 sudo service nginx start
 cd /home/box/web
-gunicorn hello:app --config etc/gunicorn.py
+gunicorn hello:app --config etc/gunicorn.py -D
