8ff9388eacd8542eb026d7d6b8b9420d7ecc5619
Koder Sohm <k0der85@mail.ru>
2018-10-06 01:25:51 +0300

    fixed models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Question(models.Model)
+class Question(models.Model):
 	title = models.CharField(maxlength=255)
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User, null=False, on_delete=models.SET_NULL)
-	likes = models.ManyToManyField(User)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User, null=True)
 	
-class Answer(models.Model)
+class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	question = models.ForeignKey(Question)
-	author = models.ForeignKey(User)
+	author = models.ForeignKey(User, null=True)
 	
