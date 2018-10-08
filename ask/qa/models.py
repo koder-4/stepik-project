3cd603a22868cfedab5d77f6fff1299c97dc9345
Koder Sohm <k0der85@mail.ru>
2018-10-08 14:23:35 +0300

    QuestionManager added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return super(QuestionManager, self).get_queryset().order_by('-added_at')
+
+    def popular(self):
+        return super(QuestionManager, self).get_queryset().annotate(models.Count('likes')).order_by('likes__count')
+		
+
 class Question(models.Model):
 	title = models.CharField(maxlength=255)
 	text = models.TextField()
