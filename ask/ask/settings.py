a00e44c8a2c1b03b48e9fc33c01c02a1c35ee91e
Koder Sohm <k0der85@mail.ru>
2018-11-09 14:59:00 +0300

    logging added

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -134,3 +134,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Logging:
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
